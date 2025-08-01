f3aaa4a9e6bc6028a161f00226915a7f4425cd9b
narielponce <narielponce@gmail.com>
2025-07-31 21:13:16 -0300

    Agregado nuevo modelo template y landing page

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,13 @@
 # core/admin.py
 from django.contrib import admin
-from .models import Cliente
+from .models import Cliente, ConfirmacionAsistencia, CancionPlaylist, Template
 
 @admin.register(Cliente)
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ("nombre", "slug", "tipo_evento", "fecha_evento")
+    prepopulated_fields = {"slug": ("nombre",)}
+
+@admin.register(Template)
+class TemplateAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'slug', 'descripcion')
     prepopulated_fields = {"slug": ("nombre",)}
