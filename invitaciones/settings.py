f29e8d600305a46211086eaff2e68dc59f94b8cf
narielponce <narielponce@gmail.com>
2025-09-08 22:05:17 -0300

    fix countdown en mobile template clasico4

--- invitaciones/settings.py
+++ invitaciones/settings.py
@@ -115,11 +115,20 @@ USE_I18N = True
 
 USE_TZ = True
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Ruta absoluta donde collectstatic copiará todos los archivos
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# (opcional: si usás archivos estáticos propios en dev)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
